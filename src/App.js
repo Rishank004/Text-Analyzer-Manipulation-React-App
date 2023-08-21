16cd116a8bd5d65debbb68ce2d31b999802fd33e
Rishank004 <004bandhu@gmai.com>
2023-08-21 23:36:30 +0530

    Add Some more features.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
+
 import './App.css';
+import About from './components/About';
 import Navbar from './components/navbar';
 import Textform from './components/Textform';
 
@@ -9,6 +10,7 @@ function App() {
    <Navbar/>
    <div className= "container my-3">
     <Textform heading="Enter the text area to analyze"/>
+    {/* <About/> */}
    </div>
     </>
   );
